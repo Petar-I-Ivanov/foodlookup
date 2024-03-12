ca094dab900839af4f6fa66d213a7a5b4a3883e0
Petar-I-Ivanov <p.ivanov2955@gmail.com>
2024-03-12 20:18:59 +0200

    First commit

---
+++ src/main/java/bg/pufmi/foodlookup/FoodlookupApplication.java
@@ -0,0 +1,13 @@
+package bg.pufmi.foodlookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodlookupApplication.class, args);
+	}
+
+}
