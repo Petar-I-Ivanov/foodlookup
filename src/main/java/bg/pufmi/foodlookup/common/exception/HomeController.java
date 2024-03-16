fb6bb4a7c3207c6ea56d301fae652be6f265c2d3
Petar-I-Ivanov <p.ivanov2955@gmail.com>
2024-03-16 22:40:40 +0200

    added frontend-maven-plugin and configured app for prod

---
+++ src/main/java/bg/pufmi/foodlookup/common/exception/HomeController.java
@@ -0,0 +1,13 @@
+package bg.pufmi.foodlookup.common.exception;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  public String index() {
+    return "forward:/index.html";
+  }
+}
